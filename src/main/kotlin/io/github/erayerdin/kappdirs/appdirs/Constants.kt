14057f38a08813c42223782ee0e6082aefc18eca
Eray Erdin <eraygezer.94@gmail.com>
2019-03-16 00:33:45 +0300

    added WindowsAppDirs

--- src/main/kotlin/io/github/erayerdin/kappdirs/appdirs/Constants.kt
+++ src/main/kotlin/io/github/erayerdin/kappdirs/appdirs/Constants.kt
@@ -2,3 +2,8 @@ package io.github.erayerdin.kappdirs.appdirs
 
 internal val HOME_DIR = System.getProperty("user.home")
 internal val SEPARATOR = System.getProperty("file.separator")
+
+// Windows Only
+internal val APPDATA = System.getenv("APPDATA")
+internal val LOCALAPPDATA = System.getenv("LOCALAPPDATA")
+internal val PROGRAMDATA = System.getenv("PROGRAMDATA")
