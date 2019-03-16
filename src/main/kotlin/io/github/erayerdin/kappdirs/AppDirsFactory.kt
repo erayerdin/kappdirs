3812965c8a7763f8525fc85a93daf88b7f41c59c
Eray Erdin <eraygezer.94@gmail.com>
2019-03-16 21:30:22 +0300

    added docstrings for AppDirsFactory and AppDirsFactory::getInstance

--- src/main/kotlin/io/github/erayerdin/kappdirs/AppDirsFactory.kt
+++ src/main/kotlin/io/github/erayerdin/kappdirs/AppDirsFactory.kt
@@ -6,10 +6,20 @@ import io.github.erayerdin.kappdirs.appdirs.WindowsAppDirs
 
 private val osName = System.getProperty("os.name").toLowerCase()
 
+/**
+ * A factory generating `AppDirs` instance.
+ *
+ * @see AppDirs
+ */
 class AppDirsFactory {
     companion object {
         private var appDirs: AppDirs? = null
 
+        /**
+         * Provides an appropriate `AppDirs` implementation.
+         *
+         * @see AppDirs
+         */
         fun getInstance(): AppDirs {
             if (appDirs != null) {
                 return appDirs as AppDirs
