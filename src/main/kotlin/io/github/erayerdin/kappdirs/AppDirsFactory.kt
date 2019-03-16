c2007a7ebc11f1bc8d862dcaa55493711b92b4ab
Eray Erdin <eraygezer.94@gmail.com>
2019-03-16 14:21:36 +0300

    added AppDirsFactoryTest

--- src/main/kotlin/io/github/erayerdin/kappdirs/AppDirsFactory.kt
+++ src/main/kotlin/io/github/erayerdin/kappdirs/AppDirsFactory.kt
@@ -2,22 +2,24 @@ package io.github.erayerdin.kappdirs
 
 import io.github.erayerdin.kappdirs.appdirs.OSXAppDirs
 import io.github.erayerdin.kappdirs.appdirs.UnixAppDirs
+import io.github.erayerdin.kappdirs.appdirs.WindowsAppDirs
 
 private val osName = System.getProperty("os.name").toLowerCase()
 
 class AppDirsFactory {
     companion object {
-        var appDirs: AppDirs? = null
+        private var appDirs: AppDirs? = null
 
         fun getInstance(): AppDirs {
             if (appDirs != null) {
                 return appDirs as AppDirs
             }
 
-            if (osName.startsWith("mac"))
-                appDirs = OSXAppDirs()
-            else
-                appDirs = UnixAppDirs()
+            when {
+                osName.startsWith("windows") -> appDirs = WindowsAppDirs()
+                osName.startsWith("mac") -> appDirs = OSXAppDirs()
+                else -> appDirs = UnixAppDirs()
+            }
 
             return appDirs as AppDirs
         }
