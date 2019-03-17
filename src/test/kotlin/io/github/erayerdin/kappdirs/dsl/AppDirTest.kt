8e92df85581380e6fb3b1e5d625f7f5c0cc3f879
Eray Erdin <eraygezer.94@gmail.com>
2019-03-17 23:54:26 +0300

    removed mock on AppDirTest

--- src/test/kotlin/io/github/erayerdin/kappdirs/dsl/AppDirTest.kt
+++ src/test/kotlin/io/github/erayerdin/kappdirs/dsl/AppDirTest.kt
@@ -1,12 +1,9 @@
 package io.github.erayerdin.kappdirs.dsl
 
-import io.github.erayerdin.kappdirs.appdirs.originalHome
 import io.github.erayerdin.kappdirs.appdirs.osName
-import org.junit.AfterClass
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Assume.assumeTrue
-import org.junit.BeforeClass
 import org.junit.Test
 import java.io.File
 
@@ -14,22 +11,24 @@ class AppDirTest {
     companion object {
         private val appDir = AppDir("foo", "0.1.0", "eray")
 
-        @JvmStatic
-        @BeforeClass
-        fun setUpClass() {
-            when {
-                osName!!.startsWith("mac") -> System.setProperty("user.home", "/Users/bar")
-                osName.startsWith("windows") -> {}
-                else -> System.setProperty("user.home", "/home/bar")
-            }
-            System.setProperty("user.home", "/home/bar")
-        }
-
-        @JvmStatic
-        @AfterClass
-        fun tearDownClass() {
-            System.setProperty("user.home", originalHome)
-        }
+//        @JvmStatic
+//        @BeforeClass
+//        fun setUpClass() {
+//
+//
+//            when {
+//                osName!!.startsWith("mac") -> System.setProperty("user.home", "/Users/bar")
+//                osName.startsWith("windows") -> {}
+//                else -> System.setProperty("user.home", "/home/bar")
+//            }
+//            System.setProperty("user.home", "/home/bar")
+//        }
+//
+//        @JvmStatic
+//        @AfterClass
+//        fun tearDownClass() {
+//            System.setProperty("user.home", originalHome)
+//        }
     }
 
     @Test
